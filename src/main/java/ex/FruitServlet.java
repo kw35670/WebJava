5455c981a4d5a01d18520a330cde313743b5984a
riku <tkyu58690@gmail.com>
2023-06-29 20:54:57 +0900

    dokotsubu created

--- src/main/java/ex/FruitServlet.java
+++ src/main/java/ex/FruitServlet.java
@@ -3,12 +3,12 @@ package ex;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class FruitServlet
@@ -30,8 +30,8 @@ public class FruitServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Fruit fruit = new Fruit("いちご", 700);
-		HttpSession session = request.getSession();
-		session.setAttribute("fruit", fruit);
+		ServletContext application = this.getServletContext();
+		application.setAttribute("fruit", fruit);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/ex/fruit.jsp");
 		dispatcher.forward(request, response);
 	}
